65f136e84c4715bc840c9b120af2ab3f108bf7b5
emmanoel <emmanoelporto@gmail.com>
2022-01-11 10:38:59 -0300

    New WalletForm Component and fixlint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import WalletHeader from './WalletHeader';
+import WalletForm from './WalletForm';
 
 class Wallet extends React.Component {
   render() {
     return (
       <>
         <WalletHeader />
+        <WalletForm />
         <div>TrybeWallet</div>
       </>
     );
---
+++ src/pages/WalletForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+
+class WalletForm extends React.Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default WalletForm;
