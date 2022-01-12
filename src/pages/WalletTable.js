c61169b80d02561ed0b2d38d5a2353d76ab31694
emmanoel <emmanoelporto@gmail.com>
2022-01-12 13:12:48 -0300

    Remove css import in wallet-table

--- src/pages/WalletTable.js
+++ src/pages/WalletTable.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { table } from './table.css';
 
 class WalletTable extends React.Component {
   render() {
